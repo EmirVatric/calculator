79b0993726d7fd4891bb5ed07f928ac38f4b2dcf
EmirVatric <vatricemir@gmail.com>
2019-10-01 23:23:58 +0200

    dots fix

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,30 +1,38 @@
 import Big from 'big.js'
 
 const operate = (numberOne, numberTwo, operator) => {
+  const regex = new RegExp('[.]{2,}');
 
-  let numOne = numberOne === null ? Big(0) : Big(numberOne)
-  let numTwo = numberTwo === null ? Big(0) : Big(numberTwo)
+  if (regex.test(numberOne) || regex.test(numberTwo)) {
+    return 'error'
+  } else {
+    let numOne = numberOne === null ? Big(0) : Big(numberOne)
+    let numTwo = numberTwo === null ? Big(0) : Big(numberTwo)
 
-  switch (operator) {
-    case "+":
-      return numOne.plus(numTwo).toString();
 
-    case "-":
-      return numOne.minus(numTwo).toString();
+    switch (operator) {
+      case "+":
+        return numOne.plus(numTwo).toString();
 
-    case "x":
-      return numOne.times(numTwo).toString();
+      case "-":
+        return numOne.minus(numTwo).toString();
 
-    case "/":
-      return numOne.div(numTwo).toString();
+      case "x":
+        return numOne.times(numTwo).toString();
 
-    case "%":
-      return (numOne / 100) * numTwo
+      case "/":
+        return numOne.div(numTwo).toString();
 
-    default:
-      return 'error'
+      case "%":
+        return (numOne / 100) * numTwo
 
+      default:
+        return 'error'
+
+    }
   }
+
+
 }
 
 export default operate;
