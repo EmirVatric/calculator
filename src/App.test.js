f57fed9c84deda1d261363e158563e0c88d1948f
EmirVatric <vatricemir@gmail.com>
2019-10-01 16:41:42 +0200

    milestone1

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
-import React from 'react';
+/* eslint-disable */
+import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; // eslint-disable-line no-unused-vars
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
