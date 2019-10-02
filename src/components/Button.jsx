763dd98d3026bf366c819f12cabbffd6fe993bdd
EmirVatric <vatricemir@gmail.com>
2019-10-02 17:12:20 +0200

    fixes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,12 @@ import React from 'react'; // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types'
 
 const Button = (props) => {
+
+  const clickHandler = () => props.clickHandler(props.name)
+
+
   return (
-    <button className={'button'} style={{ backgroundColor: (props.color === 'orange' ? '' : 'lightGray'), width: (props.width === true ? '50%' : '') }}>
+    <button className={'button'} style={{ backgroundColor: props.color, width: (props.width === true ? '50%' : '') }} onClick={() => clickHandler()}>
       {props.name}
     </button >
   );
@@ -13,6 +17,10 @@ Button.propTypes = {
   name: PropTypes.string.isRequired,
   width: PropTypes.bool.isRequired,
   color: PropTypes.string
+}
+
+Button.defaultProps = {
+  color: 'orange'
 }
 
 export default Button;
